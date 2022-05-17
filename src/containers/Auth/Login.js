fd6b4b72bd997849849d40a1701d79264e3459fd
khanh <khanhchallenger2086@gmail.com>
2022-05-18 02:56:03 +0700

    reactjs modal

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -15,7 +15,8 @@ class Login extends Component {
         this.state = {
             username: '',
             password: '',
-            isShowPassword: false
+            isShowPassword: false,
+            errMessage: '',
         }
     }
 
@@ -34,10 +35,28 @@ class Login extends Component {
     }
 
     handleLogin = async () => {
+        this.setState({
+            errMessage: ''
+        })
+
         try {
-            await handleLoginApi(this.state.username, this.state.password);
+            let data = await handleLoginApi(this.state.username, this.state.password);
+            if (data && data.errCode !== 0) {
+                this.setState({
+                    errMessage: data.message
+                })
+            }
+            if (data && data.errCode === 0) {
+                this.props.userLoginSuccess(data.user)
+            }
         } catch (e) {
-            console.log(e)
+            if (e.response) {
+                if (e.response.data) {
+                    this.setState({
+                        errMessage: e.response.data.message
+                    })
+                }
+            }
         }
     }
 
@@ -76,6 +95,9 @@ class Login extends Component {
                                 </span>
                             </div>
                         </div>
+                        <div className='col-12' style={{ color: 'red' }}>
+                            {this.state.errMessage}
+                        </div>
                         <div className='col-12'>
                             <button className='btn-login' onClick={() => { this.handleLogin() }}>Login</button>
                         </div>
@@ -106,8 +128,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
     return {
         navigate: (path) => dispatch(push(path)),
-        adminLoginSuccess: (adminInfo) => dispatch(actions.adminLoginSuccess(adminInfo)),
-        adminLoginFail: () => dispatch(actions.adminLoginFail()),
+        // userLoginFail: () => dispatch(actions.adminLoginFail()),
+        userLoginSuccess: (userInfo) => dispatch(actions.userLoginSuccess(userInfo))
     };
 };
 
--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,89 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import { getAllUsers } from '../../services/userService';
+import ModalUser from './ModalUser';
+
 class UserManage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: [],
+            isOpenModalUser: false
+        }
+    }
 
     state = {
 
     }
 
-    componentDidMount() {
+    async componentDidMount() {
+        let response = await getAllUsers('ALL');
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users
+            })
+        }
+    }
 
+    handleAddNewUser = () => {
+        this.setState({
+            isOpenModalUser: true
+        })
+    }
+
+    toggleModalUser = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser
+        })
     }
 
 
     render() {
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container mx-3 mt-1">
+                <ModalUser
+                    isOpen={this.state.isOpenModalUser}
+                    toggleFromModal={this.toggleModalUser}
+                />
+                <div className="title text-center"> Manage users with Khanh</div>
+                <div className='mx-1'>
+                    <button
+                        className='btn btn-primary px-3 mb-3'
+                        onClick={() => this.handleAddNewUser()}
+                    ><i className="fa fa-plus" aria-hidden="true"></i>Add new user
+                    </button>
+                </div>
+                <div className='users-table mt-1 mx-1'>
+                    <table id="customers">
+                        <tr>
+                            <th>Email</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Address</th>
+                            <th>Actions</th>
+                        </tr>
+                        {arrUsers && arrUsers.map((item, index) => {
+                            return (
+                                <tr>
+                                    <td>{item.email}</td>
+                                    <td>{item.firstName}</td>
+                                    <td>{item.lastName}</td>
+                                    <td>{item.address}</td>
+                                    <td>
+                                        <button className="btn-edit"><i className="fas fa-pencil-alt"></i></button>
+                                        <button className="btn-delete"><i className="fas fa-trash"></i></button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                        }
+
+                    </table>
+                </div>
+            </div >
         );
     }
 
